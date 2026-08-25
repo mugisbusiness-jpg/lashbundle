b66bf9f8f528c0f12ac9ea381d113421398db94d
mugisbusiness-jpg <mugisbusiness@gmail.com>
2026-08-25 15:42:31 +0300

    Update InviteGate.tsx

--- components/InviteGate.tsx
+++ components/InviteGate.tsx
@@ -38,7 +38,7 @@ export default function InviteGate({ children }: { children: React.ReactNode }) 
       <div className="inviteAura auraOne" />
       <div className="inviteAura auraTwo" />
       <section className="inviteCard">
-        <div className="inviteMonogram">LashMakers</div>
+        <div className="inviteMonogram"></div>
         <span className="kicker">PRIVATE EDUCATION</span>
         <h1>Welcome to<br/><em>Classic Course.</em></h1>
         <p>This academy is currently available by invitation only. Enter your private access code to continue.</p>
